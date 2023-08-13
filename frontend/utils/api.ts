e6e15f1d096af16ad920dbf212cc2e19c7bd8c65
kingsonseang <kingsonseang@gmail.com>
2023-08-13 22:10:15 +0100

    changed to live api

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,13 +1,13 @@
 import { create } from "apisauce";
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://192.168.89.217:8080/api",
-});
-
 // const axiosInstance = axios.create({
-//   baseURL: "https://miniture-octo-lamp.onrender.com/api",
+//   baseURL: "http://192.168.89.217:8080/api",
 // });
+
+const axiosInstance = axios.create({
+  baseURL: "https://miniture-octo-lamp.onrender.com/api",
+});
 
 const api = create({
   axiosInstance: axiosInstance,
