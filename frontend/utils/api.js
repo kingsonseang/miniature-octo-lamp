fd2a13a068d62098c8232b82e25a028330d6510a
kingsonseang <kingsonseang@gmail.com>
2023-08-16 05:26:11 +0100

    dependenct change

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -5,13 +5,8 @@ import axios from "axios";
 //   baseURL: "http://192.168.89.217:8080/api",
 // });
 
-const axiosInstance = axios.create({
-  baseURL: "https://miniture-octo-lamp.onrender.com/api",
-});
-
 const api = create({
-  axiosInstance: axiosInstance,
-  baseURL: undefined
+  baseURL: "https://miniture-octo-lamp.onrender.com/api"
 });
 
 export default api;
